591ff21150313cc257115d92fb3e9a2fb069cbfd
Ivan <je777@yandex.ru>
2025-02-02 11:52:20 +0300

    Tuples

---
+++ Tuples/PlanetCatalogue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tuples
+{
+    internal class PlanetCatalogue
+    {
+        List<Planet> _planets = new List<Planet>();
+        private byte requestQty = 0;
+        public PlanetCatalogue() 
+        { 
+            var planet = new Planet();
+
+            _planets.Add(new Planet 
+            { 
+                Name = "Venus", 
+                NumberFromSun = 2, 
+                EquatorLength = 38025, 
+                PrevousePlanet = _planets.Find(x => x.Name == "Mercury") 
+            });
+
+            _planets.Add(new Planet { 
+                Name = "Earth", 
+                NumberFromSun = 3, 
+                EquatorLength = 40075, 
+                PrevousePlanet = _planets.Find(x => x.Name == "Venus") 
+            });
+
+            _planets.Add(new Planet
+            {
+                Name = "Mars",
+                NumberFromSun =4,
+                EquatorLength = 38025,
+                PrevousePlanet = _planets.Find(x => x.Name == "Earth")
+            });
+        }
+
+        public (int NumberFromSun, int EquatorLength, string? ErrorMessage) GetPlanet(string PlanetName)
+        {
+            Planet? planet = _planets.Find(x => x.Name.ToLower() == PlanetName.ToLower());
+            string? message = null;
+            requestQty++;
+
+            if (requestQty == 3)
+            {
+                message = "You're asking too often.";
+                requestQty = 0;
+            }
+                
+
+            if (planet == null)
+                return (0, 0, "No planet found.");
+            else
+                return (planet.NumberFromSun, planet.EquatorLength, message);
+        }
+    }
+}
---
+++ Tuples/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Tuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetCatalogue = new PlanetCatalogue();
+            (int NumberFromSun, int EquatorLength, string message) result;
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Enter planet name: ");
+                var input = Console.ReadLine();
+
+                Console.WriteLine("Searching info for planet \"{0}\"...", input);
+                result = planetCatalogue.GetPlanet(input);
+                if (result.message != "No planet found.")
+                {
+                    Console.WriteLine("Planet \"{0}\" info: the order number from the Sun is {1}, length of its equator is {2}k km", input, result.NumberFromSun, result.EquatorLength);
+                    Console.WriteLine("Systme message: {0}", result.message);
+                }
+                else
+                    Console.WriteLine("\nSystme message: {0}", result.message);
+
+                Console.Write("\nPress any key for continue (Esc to quit)...");
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    return;
+
+            }
+
+
+            //Console.WriteLine("Searching info for planet \"Limonia\"...");
+            //result = planetCatalogue.GetPlanet("Limonia");
+            //if (result.message != "No planet found.")
+            //{
+            //    Console.WriteLine("Planet \"Limonia\" has order from Sun {0} and the length of equator {1}k km", result.NumberFromSun, result.EquatorLength);
+            //    Console.WriteLine("System message: {0}", result.message);
+            //}
+            //else
+            //    Console.WriteLine("Systme message: {0}", result.message);
+
+            //Console.WriteLine("Searching info for planet \"Mars\"...");
+            //result = planetCatalogue.GetPlanet("Mars");
+            //if (result.message != "No planet found.")
+            //{
+            //    Console.WriteLine("Planet \"Mars\" has order from Sun {0} and the length of equator {1}k km", result.NumberFromSun, result.EquatorLength);
+            //    Console.WriteLine("Systme message: {0}", result.message);
+            //}
+            //else
+            //    Console.WriteLine("Systme message: {0}", result.message);
+
+        }
+    }
+}
