5c1eb6096c6f7e829500a056e56f51d36034305f
Ivan <je777@yandex.ru>
2025-02-13 20:04:55 +0300

    Custom dictionary

---
+++ CustomDictionary/OtusDictionary.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace CustomDictionary
+{
+    class OtusDictionary
+    {
+        private (int key, string value)[] _values;
+        private int _size = 8;
+
+        public OtusDictionary()
+        {
+            _values = new (int key, string value)[_size];
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index > _size)
+                {
+                    return $"dictionary has {_size} elements. it's more than entered {index}";
+                };
+
+                return _values[index].value ?? "Element by the index not found";
+            }
+        }
+
+
+        public void Add(int key, string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Value is null");
+            }
+
+            var isKeyExist = _values[GetHash(key, _size)].key == key;
+
+            if (isKeyExist)
+            {
+                return;
+            }
+
+            while (!TryAdd(key, value))
+            {                
+                ExtendAndRecalculate(_size * 2);
+            };
+        }
+
+        private void ExtendAndRecalculate(int newSize)
+        {
+            var oldSize = _size;
+            var newValues = new (int key, string value) [newSize];
+
+            for (int i = 0; i < oldSize; i++)
+            {
+                var isValueEmpty = string.IsNullOrEmpty(_values[i].value) && _values[i].key == 0;
+                if (isValueEmpty)
+                {
+                    continue;
+                }
+
+                var newIndex = GetHash(_values[i].key, newSize);
+                newValues[newIndex] = _values[i];
+            }
+            _values = newValues;
+            _size = newSize;
+        }
+
+        private bool TryAdd(int key, string value)
+        {
+            var index = GetHash(key, _size);
+            var isValueEmpty = string.IsNullOrEmpty(_values[index].value) && _values[index].key == 0;
+
+            if (isValueEmpty)
+            {
+                _values[index].value = value;
+                _values[index].key = key;
+                return true;
+            }
+            return false;            
+        }
+
+        public string Get(int key)
+        {
+            var index = GetHash(key, _size);
+            return index <= _size ? _values[index].value : null! ;
+        }
+
+        private int GetHash(int key, int size)
+        {
+            return key % size;
+        }
+    }
+}
--- CustomDictionary/Program.cs
+++ CustomDictionary/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            var test1 = new OtusDictionary();
+            var random = new Random();
+
+            while (true)
+            {
+                Console.Write("\nEnter key (int): ");
+                var key = Int32.Parse(Console.ReadLine() ?? "0");
+
+                Console.Write("Enter value (string): ");
+                var value = Console.ReadLine() ?? "";
+
+                test1.Add(key, value);
+
+                Console.Write("\nEnter key (int) to retrieve value: ");
+                var  input = Int32.Parse(Console.ReadLine() ?? "0");
+                Console.WriteLine($"Value: {test1.Get(input) ?? "key not found"}");
+
+                Console.Write("\nEnter index (int) to retrieve value: ");
+                input = Int32.Parse(Console.ReadLine() ?? "0");
+                Console.WriteLine($"Value by index: {test1[input]}");
+
+            }
         }
     }
 }
