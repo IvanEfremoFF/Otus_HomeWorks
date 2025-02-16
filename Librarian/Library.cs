4906cc4c44ea7c56c575225a46b8fe8e871f700a
Ivan <je777@yandex.ru>
2025-02-16 22:16:23 +0300

    Librarian

---
+++ Librarian/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarian
+{
+    internal class Library
+    {
+        private ImmutableDictionary<string, int> _books;
+
+        public Library() 
+        {
+            _books = ImmutableDictionary.Create<string, int>();
+        }
+
+        public void AddBook(string name) 
+        {
+            
+            if (_books.ContainsKey(name) || string.IsNullOrEmpty(name))
+                return;
+
+            _books = _books.Add(name, 0);
+        }
+
+        public string GetUnreadBooks()
+        {
+            var listOfUnreadBooks = "";
+            foreach (var book in _books)
+            {
+                listOfUnreadBooks += $"{book.Key} - {book.Value}%\n";
+            }
+
+            return listOfUnreadBooks;
+        }
+
+        public void RecalculatePercentage() 
+        {
+            
+            foreach (var book in _books)
+            {
+                _books = _books.Remove(book.Key);                
+                if (book.Value + 1 <= 100)
+                {
+                    _books = _books.Add(book.Key, book.Value + 1);
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Librarian/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Librarian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            var library = new Library();
+            bool stopRecalculate = false;
+
+            Thread recalculateReading = new Thread(() =>
+            {
+                while (!stopRecalculate)
+                {
+                    library.RecalculatePercentage();
+                    Thread.Sleep(1000);
+                }
+            });
+            recalculateReading.Start();
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter menu number to proceed: ");
+                Console.WriteLine("1 - add book \n2 - show list of unread books \n3 - exit");
+                var selectedMenu = Console.ReadLine();
+                
+                switch (selectedMenu)
+                {
+                    case "1":
+                        Console.Write("Enter book name: ");
+                        var bookName = Console.ReadLine();
+                        library.AddBook(bookName ?? "");
+                        break;
+                    case "2":
+                        Console.WriteLine($"List of unread books: \n{library.GetUnreadBooks()}");
+                        break;
+                }
+                
+                if (selectedMenu == "3")
+                {
+                    stopRecalculate = true;
+                    break;
+                }
+                
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+            recalculateReading.Join();
+        }
+    }
+}
